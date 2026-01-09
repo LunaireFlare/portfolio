3df82fd668e1c560f5de2b8e3298f33b0b928ac2
LunaireFlare <181491423+LunaireFlare@users.noreply.github.com>
2026-01-09 16:10:21 +0100

    build: create project model

---
+++ src/app/models/project.ts
@@ -0,0 +1,10 @@
+export class Project {
+
+  constructor(public title: string,
+              public imageUrl: string,
+              public stack: string,
+              public description: string,
+              public liveUrl: string,
+              public repoUrl: string
+            ) {}
+}
