00a0f5d9ad827bc9d2a5ae08770cf34279bdc743
LunaireFlare <181491423+LunaireFlare@users.noreply.github.com>
2026-01-09 16:12:03 +0100

    feat: integrate projects component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { Hero } from './components/hero/hero';
+import { Projects } from "./components/projects/projects";
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,7 @@ import { Hero } from './components/hero/hero';
     RouterOutlet,
     Header,
     Hero,
+    Projects
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
