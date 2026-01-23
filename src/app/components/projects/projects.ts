8104ff6d036aa35a763fdfae2112a9363e70ccce
LunaireFlare <181491423+LunaireFlare@users.noreply.github.com>
2026-01-23 15:18:40 +0100

    feat: add current project and explicit project stacks

--- src/app/components/projects/projects.ts
+++ src/app/components/projects/projects.ts
@@ -23,7 +23,7 @@ export class Projects implements OnInit {
           "assets/projects/rioval/rioval3.png", 
           "assets/projects/rioval/rioval4.png"
         ],
-        "html, tailwind, nextjs, typescript",
+        "html, tailwind, nextjs, typescript | git, github",
         "A simple and colourful website to showcase a small French food business and its recipes. It was designed to reflect the values of the company through vibrant colours and subtle animations. Fully responsive for mobile devices.",
         "https://www.rioval.fr/",
         ""
@@ -35,7 +35,7 @@ export class Projects implements OnInit {
           "assets/projects/orizon/orizon2.png", 
           "assets/projects/orizon/orizon3.png"
         ],
-        "html, sass, react, typescript, nginx, docker, postgresql",
+        "html, sass, react, typescript | nodejs, postgresql, sequelize, docker, nginx | git, github",
         "A social platform to help senior citizens maintain social links after retirement. At its core is the event system, allowing any member to create, search and participate in an event. It comes with an administrator dashboard to moderate members and events, and uses a reverse proxy to link the frontends and backend.",
         "",
         "https://github.com/LunaireFlare/orizon"
@@ -46,10 +46,20 @@ export class Projects implements OnInit {
           "assets/projects/portfolio/portfolio1.png",
           "assets/projects/portfolio/portfolio2.png"
         ],
-        "html, tailwind, angular, typescript",
+        "html, tailwind, angular, typescript | git, github",
         "A first iteration of my personal portfolio, showcasing my projects and technical stack, in order to practice Angular and OOP. It is a work in progress, subject to change, and aims to have full mobile responsiveness.",
         "#",
         "https://github.com/LunaireFlare/portfolio"
+      ),
+      new Project(
+        "Currently in development",
+        [
+          "assets/images/space.avif",
+        ],
+        "html, tailwind, angular, typescript | nodejs, postgresql, sequelize, docker | git, github",
+        "A planning & organisation app for personal and professional trips.",
+        "",
+        ""
       ),
     ]
   }
